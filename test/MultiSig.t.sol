cc4cad9ce4d5c437d804f6adb59cc036e672499e
Feyisara2108 <ayokomi2108@gmail.com>
2026-02-27 16:56:36 +0100

    feat: updates the contract test

--- test/MultiSig.t.sol
+++ test/MultiSig.t.sol
@@ -3,38 +3,67 @@ pragma solidity ^0.8.28;
 
 import "forge-std/Test.sol";
 import "../src/MultiSig.sol";
-import "../src/MultiSigFactory.sol";
 
-contract MultiSigTest is Test {
+contract MultiSigBehaviorTest is Test {
     MultiSig multiSig;
-    MultiSigFactory factory;
+    address[] public owners;
 
     address owner1 = address(1);
     address owner2 = address(2);
     address owner3 = address(3);
-
+    address nonOwner = address(4);
     address recipient = address(99);
 
     function setUp() public {
-        address[] memory owners = new address[](3);
+        owners = new address[](3);
         owners[0] = owner1;
         owners[1] = owner2;
         owners[2] = owner3;
 
         multiSig = new MultiSig(owners, 2);
-        factory = new MultiSigFactory();
 
         vm.deal(address(multiSig), 10 ether);
     }
 
-    function testSubmitTransaction() public {
+    function testNonOwnerCannotSubmit() public {
+        vm.prank(nonOwner);
+        vm.expectRevert(MultiSig.NotOwner.selector);
+        multiSig.submit(recipient, 1 ether);
+    }
+
+    function testOwnerCanApprove() public {
         vm.prank(owner1);
         multiSig.submit(recipient, 1 ether);
 
-        assertEq(multiSig.getTransactionCount(), 1);
+        vm.prank(owner1);
+        multiSig.approve(0);
+
+        (,,, uint256 approvalCount) = multiSig.transactions(0);
+        assertEq(approvalCount, 1);
     }
 
-    function testApproveAndExecute() public {
+    function testCannotApproveTwice() public {
+        vm.startPrank(owner1);
+        multiSig.submit(recipient, 1 ether);
+        multiSig.approve(0);
+
+        vm.expectRevert(MultiSig.TxAlreadyApproved.selector);
+        multiSig.approve(0);
+        vm.stopPrank();
+    }
+
+    function testCannotExecuteWithoutEnoughApprovals() public {
+        vm.startPrank(owner1);
+        multiSig.submit(recipient, 1 ether);
+        multiSig.approve(0);
+        vm.stopPrank();
+
+        vm.prank(owner1);
+        vm.expectRevert(MultiSig.NotEnoughApprovals.selector);
+        multiSig.execute(0);
+    }
+
+    function testExecuteAfterEnoughApprovals() public {
         vm.startPrank(owner1);
         multiSig.submit(recipient, 1 ether);
         multiSig.approve(0);
@@ -51,13 +80,14 @@ contract MultiSigTest is Test {
         assertEq(recipient.balance, balanceBefore + 1 ether);
     }
 
-    function testFactoryCreatesWallet() public {
-        address;
-        owners[0] = owner1;
-        owners[1] = owner2;
+    function testOwnerCanRevokeApproval() public {
+        vm.startPrank(owner1);
+        multiSig.submit(recipient, 1 ether);
+        multiSig.approve(0);
+        multiSig.revoke(0);
+        vm.stopPrank();
 
-        address wallet = factory.createWallet(owners, 2);
-
-        assertTrue(wallet != address(0));
+        (,,, uint256 approvalCount) = multiSig.transactions(0);
+        assertEq(approvalCount, 0);
     }
 }
